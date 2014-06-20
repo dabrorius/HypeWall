04a7b53403de822a38806ff916272dcd917abb71
Filip Defar <filip@codemonster.io>
2014-06-20 15:24:30 +0200

    Added jQuery method for replacing images in grid

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,16 @@
 function animatePhoto(photoId, animation) {
-  $("#photo-frame-"+photoId).removeClass().addClass('photo-frame animated ' + animation).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-    $(this).removeClass().addClass("photo-frame");
+  $("#photo-"+photoId).removeClass().addClass('animated ' + animation).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    $(this).removeClass();
   });
 }
 
+
+function pushImage(position, url, animation) {
+  var frameSelector = "#photo-frame-"+position;
+  $(frameSelector + " img").addClass("to-remove");
+  $(frameSelector).append("<img class='animated "+ animation +"' src='"+url+"'>").
+  one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
+    function(){
+      $(frameSelector + " .to-remove").remove();
+    });
+}
