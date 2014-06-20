40d9120910efac2d8a1f70b01e8fb1fa81d413fb
Filip Defar <filip@codemonster.io>
2014-06-20 12:36:35 +0200

    Added basic wall layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,6 @@ class PagesController < ApplicationController
   end
 
   def wall
+    @photos = ['/stock/1.jpg', '/stock/2.jpg', '/stock/3.jpg', '/stock/4.jpg', '/stock/5.jpg', '/stock/6.jpg', '/stock/7.jpg', '/stock/8.jpg', '/stock/9.jpg']
   end
 end
