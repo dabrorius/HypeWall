c5a2873ee4f9b80e89068b40e5296d1d18fa9c62
Filip Defar <filip@codemonster.io>
2014-06-21 13:12:47 +0200

    Added photo zooming

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,10 +1,16 @@
+var verticalAnchors = ['anchor-left','anchor-right'];
+var horizontalAnchors = ['anchor-up','anchor-down'];
+
 function pushImage(position, url, animation) {
   var frameSelector = "#photo-frame-"+position;
   $(frameSelector + " img").addClass("to-remove");
-  $(frameSelector).append("<img class='animated "+ animation +"' src='"+url+"'>").
+  var verticalAnchor = verticalAnchors[Math.floor(Math.random()*verticalAnchors.length)];
+  var horizontalAnchor = horizontalAnchors[Math.floor(Math.random()*horizontalAnchors.length)];
+  $(frameSelector).append("<img class='animated "+ animation +" "+ verticalAnchor +" "+ horizontalAnchor +"' src='"+url+"'>").
   one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
     function(){
       $(frameSelector + " .to-remove").remove();
+      $(frameSelector + " img").animate({height: "110%"}, 5000)
     });
 }
 
@@ -18,4 +24,4 @@ function fetchNextImage() {
   });
 }
 
-setInterval(fetchNextImage, 3000);
+setInterval(fetchNextImage, 5000);
