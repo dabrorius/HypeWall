1a23c96259940c9eb73989fdb4530117b88677aa
Ivan Lučev <lucev.ivan@gmail.com>
2014-06-23 14:12:02 +0200

    Add image likes

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,16 +1,21 @@
 class Image < ActiveRecord::Base
 
   def self.next
+    image = Image.first
     new_images = Image.where('presented = ?', false).order('created_at ASC')
     if new_images.present?
       image = new_images.first
       image.update_attributes(presented: true)
-      return image
     else
       last_fetched = Rails.cache.fetch('last_fetched') || 0
       current_image = (last_fetched + 1) % Image.count;
       Rails.cache.write('last_fetched', current_image)
-      return Image.offset(current_image).first
+      image = Image.offset(current_image).first
     end
+    unless image.original_id.nil?
+      likes = Instagram.media_likes(image.original_id).count
+      image.update_attributes(likes: likes)
+    end
+    return image
   end
 end
