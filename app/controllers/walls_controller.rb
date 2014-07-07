3e880757ace7257fe9c8ad097e04fd59db2b573f
Filip Defar <filip@codemonster.io>
2014-07-07 22:55:03 +0200

    Only current user's walls are shown in the scaffold

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -1,10 +1,11 @@
 class WallsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_wall, only: [:show, :edit, :update, :destroy]
 
   # GET /walls
   # GET /walls.json
   def index
-    @walls = Wall.all
+    @walls = current_user.walls
   end
 
   # GET /walls/1
@@ -28,6 +29,7 @@ class WallsController < ApplicationController
 
     respond_to do |format|
       if @wall.save
+        WallRole.create(user: current_user, wall: @wall)
         format.html { redirect_to @wall, notice: 'Wall was successfully created.' }
         format.json { render :show, status: :created, location: @wall }
       else
@@ -64,7 +66,7 @@ class WallsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_wall
-      @wall = Wall.find(params[:id])
+      @wall = current_user.walls.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :wall_roles
+  has_many :walls, through: :wall_roles
 end
