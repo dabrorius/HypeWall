72fec5260fb4db138848ba96ed211a119f4a656c
Filip Defar <filip@codemonster.io>
2014-08-21 13:39:18 +0200

    Landing page improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def websocket_host
     ENV['WEBSOCKET_HOST']
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
