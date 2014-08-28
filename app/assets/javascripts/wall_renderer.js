0569b2eab4f7e1fa1075fa04bee5cda388a60d8d
Filip Defar <filip@codemonster.io>
2014-08-28 15:49:53 +0200

    Introduced create image

--- app/assets/javascripts/wall_renderer.js
+++ app/assets/javascripts/wall_renderer.js
@@ -4,72 +4,59 @@ $(document).ready(function(){// Get the canvas element from our HTML above
   // Load the BABYLON 3D engine
   var engine = new BABYLON.Engine(canvas, true);
 
-  var moveToFarRight = function(image, scene) {
-    var playgroundHalfWidth = 30;
-
-    // Animating it
-    var animation = new BABYLON.Animation(
-      "tutoAnimation",
-      "position",
-      15,
+  var moveTo = function(image, scene, newX, newZ, newRotation) {
+    var positionAnimation = new BABYLON.Animation(
+      "tutoAnimation", "position", 15,
       BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
       BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
-    var keys = [];  
-    keys.push({ frame: 0, value: new BABYLON.Vector3(playgroundHalfWidth,0,20) });
-    keys.push({ frame: 10, value: new BABYLON.Vector3(15,0,20) });
-    animation.setKeys(keys);
-    image.animations.push(animation);
+    positionAnimation.setKeys([
+      { frame: 0, value: image.position }, 
+      { frame: 10, value: new BABYLON.Vector3(newX,0,newZ) }
+    ]);
+    image.animations.push(positionAnimation);
 
-    // Animating it
-    var animation2 = new BABYLON.Animation(
-      "tutoAnimation",
-      "rotation.y",
-      15,
+    var rotationAnimation = new BABYLON.Animation(
+      "tutoAnimation", "rotation.y", 15,
       BABYLON.Animation.ANIMATIONTYPE_FLOAT,
       BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
-    var keys2 = [];  
-    keys2.push({ frame: 0, value: 2.5 });
-    keys2.push({ frame: 10, value: 1 });
-    animation2.setKeys(keys2);
-    image.animations.push(animation2);
+    rotationAnimation.setKeys([
+      { frame: 0, value: image.rotation.y },
+      { frame: 10, value: newRotation }
+    ]);
+    image.animations.push(rotationAnimation);
 
     scene.beginAnimation(image, 0, 15, true);
   }
 
-  var createScene = function () {
+  var createImage = function(url, scene) {
+    var newImage = BABYLON.Mesh.CreatePlane("newImage", 10.0, scene);
+    newImage.position.z = 10;
+    var planeMaterial = new BABYLON.StandardMaterial("texture1", scene);
+    newImage.material = planeMaterial;
+    // planeMaterial.diffuseColor = new BABYLON.Color3(1.0, 0.2, 0.7);
+    newImage.material.diffuseTexture = new BABYLON.Texture(url, scene);
+    return newImage;
+  }
 
+  var createScene = function () {
     // Now create a basic Babylon Scene object 
     var scene = new BABYLON.Scene(engine);
 
     // This creates and positions a free camera
     var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 0, -10), scene);
-
-    // This targets the camera to scene origin
     camera.setTarget(new BABYLON.Vector3.Zero());
-
-    // This attaches the camera to the canvas
     camera.attachControl(canvas, false);
 
     // This creates a light, aiming 0,1,0 - to the sky.
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
-    // Dim the light a small amount
     light.intensity = 1;
 
-    var plane = BABYLON.Mesh.CreatePlane("plane", 10.0, scene);
-    plane.position.z = 10;
-
-    var planeMaterial = new BABYLON.StandardMaterial("texture1", scene);
-    plane.material = planeMaterial;
-    // planeMaterial.diffuseColor = new BABYLON.Color3(1.0, 0.2, 0.7);
-    planeMaterial.diffuseTexture = new BABYLON.Texture("/ultra/1.jpg", scene);
-
-    moveToFarRight(plane, scene);
-
+    // Create image
+    var plane = createImage("/ultra/1.jpg", scene);
+    moveTo(plane, scene, 15, 20, -1);
 
     // Leave this function
     return scene;
-
   };  // End of createScene function
 
   var scene = createScene();
