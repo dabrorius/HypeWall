9d5fe2f9be546244992ea2631dd828acb91ed49c
nevenRakonic <neven.rakonic@gmail.com>
2014-09-05 15:06:30 +0200

    fixed migration to contain default value

--- db/migrate/20140905124301_add_is_admin_to_user.rb
+++ db/migrate/20140905124301_add_is_admin_to_user.rb
@@ -1,6 +1,6 @@
 class AddIsAdminToUser < ActiveRecord::Migration
   def up
-  	add_column :users, :is_admin, :boolean  	
+  	add_column :users, :is_admin, :boolean, default: true  	
   end
 
   def down
