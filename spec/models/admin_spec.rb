0bbb0acae4c771dd0a487aa36c1f35e7e2a9ef7b
nevenRakonic <neven.rakonic@gmail.com>
2014-09-05 18:13:07 +0200

    non signed in test for admin interface

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Admin::DashboardController, :type => :controller do
-  context "default user" do
-    let(:user) {FactoryGirl.create :user}
+  context "non signed in user" do
+    #let(:user) {FactoryGirl.create :user}
 
     before { get :index }
 
     it "should redirect to root" do
-     expect(response).to render_template("landing")
+     expect(response).to redirect_to(root_path)
     end
   end  
 end
