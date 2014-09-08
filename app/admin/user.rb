a7bcc023a1ba10aaf22a7a30b409896503eea41e
nevenRakonic <neven.rakonic@gmail.com>
2014-09-08 13:01:58 +0200

    configured user crud

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -14,5 +14,24 @@ ActiveAdmin.register User do
   #   permitted
   # end
 
+  to_permit = [:email, :subscription_level, :is_admin,
+    :sign_in_count, :created_at, :updated_at]
+
+  permit_params to_permit
+
+  index do
+    # creates an appropriate column in the index view
+    to_permit.each { |field| column field }
+    # allows crud actions
+    actions
+  end
+
+  form do |f|
+    f.inputs "User details" do
+      to_permit.each { |field| f.input field }
+    end
+    f.actions
+  end
+
 
 end
