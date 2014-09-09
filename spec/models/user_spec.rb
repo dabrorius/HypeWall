3fb60d7875c10961dd5c089688ab72b8f70b1659
nevenRakonic <neven.rakonic@gmail.com>
2014-09-09 13:20:16 +0200

    added tests to check that wall role doesn't delete user and wall

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,14 +10,27 @@ describe User do
       expect { user.destroy }.
       to change{ WallRole.count }.from(1).to(0)
     end
-  end
 
+    it "wall role is properly removed" do
+      expect { wall_role.destroy }.
+      to change{ WallRole.count }.from(1).to(0)
+    end
+
+    it "user is not deleted when wall role is" do
+      expect { wall_role.destroy }.
+      to_not change{ User.count }
+    end
+
+    it "wall is deleted when wall role is" do
+      expect { wall_role.destroy }.
+      to_not change{ Wall.count }
+    end
+
+  end
 
   it "walls belonging to the user are deleted too" do
     user.walls << wall
     expect { user.destroy }.
     to change{ Wall.count }.from(1).to(0)
   end
-
-
 end
