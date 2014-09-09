5006407b70451b336b244e145ebb18c915b24081
Dora <dora2411@gmail.com>
2014-09-09 14:52:39 +0200

    Added console testing method

--- app/models/twitter_item.rb
+++ app/models/twitter_item.rb
@@ -1,6 +1,10 @@
 class TwitterItem < Item
   require 'tweetstream'
   
-  
+  def self.print_status
+    TweetStream::Client.new.track('#paris', '#london', '#newyork') do |status|
+      puts "#{status.text}"
+    end
+  end
   
 end
