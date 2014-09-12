8ca3f659612e6c6983bf2df22aca3ded5ac6e8d0
Neven <neven@nerk711.(none)>
2014-09-12 18:51:20 +0200

    permissions on item#approve, item#ban, and complementary tests

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,33 +3,6 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-
     alias_action :read, :update, :destroy, :to => :rud
 
     if user.is_admin?
@@ -41,6 +14,13 @@ class Ability
       can [:rud, :frame, :control, :remove_background, :remove_logo], Wall do |wall|
         user.walls.pluck(:id).include? wall.id
       end
+
+      can [:approve, :ban], Item do |item|
+        user.items.pluck(:id).include? item.id
+      end
+      # can [:create], Item do |wall|
+      #   user.walls.pluck(:id).include? wall.id
+      # end
     end
   end
 end
