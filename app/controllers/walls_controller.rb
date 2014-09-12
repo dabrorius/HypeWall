bf325682f93b2ef805ed07825ab71bf9fdbf8652
Dora <dora2411@gmail.com>
2014-09-12 16:34:03 +0200

    Ban all items from banned user

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -82,7 +82,12 @@ class WallsController < ApplicationController
     item = Item.find(params[:item_id])
     if BannedUser.where("user_id = ? AND wall_id = ?", item.user_id, item.wall_id).first.blank?
       BannedUser.create(user_id: item.user_id, wall_id: item.wall_id)
+      Item.where("user_id = ? AND wall_id = ?", item.user_id, item.wall_id).each do |i|
+        i.status = 'banned'
+        i.save
+      end
     end
+
     render :nothing => true
   end
 
