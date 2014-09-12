2bf50c7a69a97ee52a98a2e822e9f5273b908fa4
Neven <neven@nerk711.(none)>
2014-09-12 18:51:20 +0200

    permissions on item#approve, item#ban, and complementary tests

--- spec/controllers/items_spec.rb
+++ spec/controllers/items_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ItemsController, :type => :controller do
   context "when wall and item pending approval exist" do
     let(:wall) { FactoryGirl.create :wall }
-    let(:item) { FactoryGirl.create :item, status: "pending_approval" }
+    let(:item) { FactoryGirl.create :item, wall: wall, status: "pending_approval" }
 
     context "non signed in user" do
 
@@ -56,5 +56,36 @@ RSpec.describe ItemsController, :type => :controller do
         expect(response).to redirect_to(root_path)
       end
     end
+
+    context "owner" do
+      let(:owner) { FactoryGirl.create :user }
+      before do
+        sign_in owner
+        owner.walls << wall
+      end
+
+      it "can do item#approve" do
+        xhr :get, :approve, id: item
+        expect(item.reload.status).to eq "approved"
+        expect(response).to render_template("change_status")
+      end
+
+      it "can do item#ban" do
+        xhr :get, :ban, id: item
+        expect(item.reload.status).to eq "banned"
+        expect(response).to render_template("change_status")
+      end
+
+      it "can do item#new", :pending => true do
+        get :new, id: wall.slug
+        expect(response).to render_template("new")
+      end
+
+      it "can do item#create", :pending => true do
+        post :create, id: wall.slug
+        expect(response).to redirect_to control_wall_path(wall)
+      end
+    end
+
   end
 end
