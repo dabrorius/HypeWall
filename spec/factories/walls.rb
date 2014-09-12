bd0bcca3814e20aafcd2145754f7581410e7c43b
nevenRakonic <neven.rakonic@gmail.com>
2014-09-12 13:35:46 +0200

    refactored tests

--- spec/factories/walls.rb
+++ spec/factories/walls.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :wall do
     sequence(:name) {|n| "Wall #{n}" }
     sequence(:hashtag) { |n| "hashtag#{n}" }
+    sequence(:background_image_file_name) { |n| "image_name#{n}"}
+    sequence(:logo_file_name) { |n| "logo_name#{n}"}
   end
 end
