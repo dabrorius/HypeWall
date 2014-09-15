53f7699f56865e76632810a8ad711efb9de3953a
nevenRakonic <neven.rakonic@gmail.com>
2014-09-15 17:39:57 +0200

    refactored tests and changed item creation permissions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
-  load_and_authorize_resource
+  load_and_authorize_resource except: [:new, :create]
   # before_action :set_item, only: [:approve, :ban]
 
   def approve
@@ -15,15 +15,23 @@ class ItemsController < ApplicationController
 
   def new
     @wall = Wall.friendly.find(params[:id])
+    # this is used instead of cancan for authorization, because cancan has problems with nested resources
+    unless check_wall_permission
+      redirect_to root_path
+    end
   end
 
   def create
-    @wall = current_user.walls.friendly.find(params[:id])
-    params[:images_attributes].each_with_index do |image, index|
-      UploadedImage.create(uploaded_image_params(index).merge(status: 'approved').merge(wall: @wall))
+    @wall = Wall.friendly.find(params[:id])
+    unless check_wall_permission
+      redirect_to root_path
+    else
+      params[:images_attributes].each_with_index do |image, index|
+        UploadedImage.create(uploaded_image_params(index).merge(status: 'approved').merge(wall: @wall))
+      end
+      # UploadedImage.create(uploaded_image_params.merge(status: 'approved').merge(wall: @wall))
+      redirect_to control_wall_path(@wall)
     end
-    # UploadedImage.create(uploaded_image_params.merge(status: 'approved').merge(wall: @wall))
-    redirect_to control_wall_path(@wall)
   end
 
   private
@@ -34,5 +42,9 @@ class ItemsController < ApplicationController
 
     def uploaded_image_params(index)
       params.require(:images_attributes)[index].permit(:attachment)
+    end
+
+    def check_wall_permission
+      current_user.is_admin? || @wall.users.pluck(:id).include?(current_user.id)
     end
 end
--- spec/controllers/items_spec.rb
+++ spec/controllers/items_spec.rb
@@ -1,22 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, :type => :controller do
-  context "when wall and item pending approval exist" do
+  context "when wall and item pending approval with attachment exist" do
     let(:wall) { FactoryGirl.create :wall }
-    let(:item) { FactoryGirl.create :item, wall: wall, status: "pending_approval" }
+    let!(:item) { FactoryGirl.create :item, wall: wall, status: "pending_approval" }
+    let(:attachment) { Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/fake.jpg", "image/jpg") }
 
     context "non signed in user" do
 
       it "can't do item#approve" do
-        get :approve, id: item
+        xhr :get, :approve, id: item
         expect(item.reload.status).to eq "pending_approval"
-        expect(response).to redirect_to(new_user_session_path)
+        # ajax request doesn't need a redirect
+        expect(response.status).to eq 401
       end
 
       it "can't do item#ban" do
-        get :ban, id: item
+        xhr :get, :ban, id: item
         expect(item.reload.status).to eq "pending_approval"
-        expect(response).to redirect_to(new_user_session_path)
+        expect(response.status).to eq 401
       end
 
       it "can't do item#new" do
@@ -35,13 +37,13 @@ RSpec.describe ItemsController, :type => :controller do
       before { sign_in user }
 
       it "can't do item#approve" do
-        get :approve, id: item
+        xhr :get, :approve, id: item
         expect(item.reload.status).to eq "pending_approval"
         expect(response).to redirect_to(root_path)
       end
 
       it "can't do item#ban" do
-        get :ban, id: item
+        xhr :get, :ban, id: item
         expect(item.reload.status).to eq "pending_approval"
         expect(response).to redirect_to(root_path)
       end
@@ -76,13 +78,43 @@ RSpec.describe ItemsController, :type => :controller do
         expect(response).to render_template("change_status")
       end
 
-      it "can do item#new", :pending => true do
+      it "can do item#new" do
+        get :new, id: wall.id
+        expect(response).to render_template("new")
+      end
+
+      it "can do item#create" do
+        post :create, id: wall.slug, images_attributes: [attachment: attachment]
+        expect(response).to redirect_to control_wall_path(wall)
+      end
+    end
+
+    context "admin" do
+      let(:admin) { FactoryGirl.create :user, :admin }
+
+      before do
+        sign_in admin
+      end
+
+      it "can do item#approve" do
+        xhr :get, :approve, id: item
+        expect(item.reload.status).to eq "approved"
+        expect(response).to render_template("change_status")
+      end
+
+      it "can do item#ban" do
+        xhr :get, :ban, id: item
+        expect(item.reload.status).to eq "banned"
+        expect(response).to render_template("change_status")
+      end
+
+      it "can do item#new" do
         get :new, id: wall.slug
         expect(response).to render_template("new")
       end
 
-      it "can do item#create", :pending => true do
-        post :create, id: wall.slug
+      it "can do item#create" do
+        post :create, id: wall.id, images_attributes: [attachment: attachment]
         expect(response).to redirect_to control_wall_path(wall)
       end
     end
