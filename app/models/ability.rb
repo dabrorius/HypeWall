5d7275c4c1dc268e018d9caf3b30b550fc01987a
nevenRakonic <neven.rakonic@gmail.com>
2014-09-15 11:54:56 +0200

    implemented special permissions for example wall

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,11 @@ class Ability
     user ||= User.new
     alias_action :read, :update, :destroy, :to => :rud
 
+    #special case for example wall
+    can [:show, :frame], Wall do |wall|
+      wall.id == Wall.example.id if Wall.example
+    end
+
     if user.is_admin?
       can :manage, :all
     end
@@ -14,7 +19,6 @@ class Ability
       can [:rud, :frame, :control, :remove_background, :remove_logo], Wall do |wall|
         user.walls.pluck(:id).include? wall.id
       end
-
       can [:approve, :ban], Item do |item|
         user.items.pluck(:id).include? item.id
       end
