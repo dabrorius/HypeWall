89132230244bd052648a6abb54995a8318a74ee2
Dora <dora2411@gmail.com>
2014-09-16 16:42:58 +0200

    Removed controller test

--- spec/controllers/banned_user_spec.rb
+++ spec/controllers/banned_user_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
+#require 'rails_helper'
 
-RSpec.describe WallsController do
+#RSpec.describe WallsController do
 
-	describe "ban user" do
-		it "creates new banned user" do
-			item = FactoryGirl.create(:item)
-			expect{
-				post :ban_user, id: "bla", item_id: item, banned_user: FactoryGirl.attributes_for(:banned_user)
-			}.to change(BannedUser, :count).by(1)
-		end	
-	end
-end
+#	describe "ban user" do
+#		it "creates new banned user" do
+#			item = FactoryGirl.create(:item)
+#			expect{
+#				post :ban_user, id: "bla", item_id: item, banned_user: FactoryGirl.attributes_for(:banned_user)
+#			}.to change(BannedUser, :count).by(1)
+#		end	
+#	end
+#end
