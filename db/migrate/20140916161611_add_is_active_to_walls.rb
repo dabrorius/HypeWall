d32755533e636e8b2ad611df2a72092ef2e4339e
nevenRakonic <neven.rakonic@gmail.com>
2014-09-16 18:22:38 +0200

    removed previous migration

--- db/migrate/20140916161611_add_is_active_to_walls.rb
+++
@@ -1,9 +0,0 @@
-class AddIsActiveToWalls < ActiveRecord::Migration
-  def up
-    add_column :walls, :is_active, :boolean, default: false
-  end
-
-  def down
-    remove_column :walls, :is_active, :boolean
-  end
-end
