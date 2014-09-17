49d2ce39c5551b2b53e1fb2e594e53728e6ad345
nevenRakonic <neven.rakonic@gmail.com>
2014-09-17 11:23:07 +0200

    changed check_wall_permission in items_controller to be called with before_filter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_wall_permission, only: [:new, :create]
   load_and_authorize_resource except: [:new, :create]
-  # before_action :set_item, only: [:approve, :ban]
 
   def approve
     @item.approve
@@ -14,24 +14,13 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @wall = Wall.friendly.find(params[:id])
-    # this is used instead of cancan for authorization, because cancan has problems with nested resources
-    unless check_wall_permission
-      redirect_to root_path
-    end
   end
 
   def create
-    @wall = Wall.friendly.find(params[:id])
-    unless check_wall_permission
-      redirect_to root_path
-    else
-      params[:images_attributes].each_with_index do |image, index|
-        UploadedImage.create(uploaded_image_params(index).merge(status: 'approved').merge(wall: @wall))
-      end
-      # UploadedImage.create(uploaded_image_params.merge(status: 'approved').merge(wall: @wall))
-      redirect_to control_wall_path(@wall)
+    params[:images_attributes].each_with_index do |image, index|
+      UploadedImage.create(uploaded_image_params(index).merge(status: 'approved').merge(wall: @wall))
     end
+    redirect_to control_wall_path(@wall)
   end
 
   private
@@ -40,7 +29,11 @@ class ItemsController < ApplicationController
       params.require(:images_attributes)[index].permit(:attachment)
     end
 
+    # this is used instead of cancan for authorization, because cancan has problems with nested resources
     def check_wall_permission
-      current_user.is_admin? || @wall.users.pluck(:id).include?(current_user.id)
+      @wall = Wall.friendly.find(params[:id])
+      unless current_user.is_admin? || @wall.users.pluck(:id).include?(current_user.id)
+        redirect_to root_path
+      end
     end
 end
